0f4c0c1622d29e99d5d8e3d8e085ea7f68641639
ponko23 <kohe.mizu@gmail.com>
2019-09-07 23:02:42 +0900

    2.1 finished

---
+++ coursecontents/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const Header = ({title}) =>{
+    //debugger
+    return (
+      <h1>{title}</h1>
+    )
+}
+  
+const Part = ({ key, text, exercises }) => {
+    //debugger
+    return(
+      <p key={key}>{text} {exercises}</p>
+    )
+}
+  
+const Content = ({content}) => {
+    //debugger
+    return (
+      content.map(part =>
+        <Part
+          key={part.id}
+          text={part.name}
+          exercises={part.exercises}
+        />)
+    )
+}
+  
+const Course = ({ course }) => {
+    //debugger
+    return (
+      <>
+        <Header title={course.name} />
+        <Content content={course.parts} />
+      </>
+    )
+}
+  
+const App = () => {
+    const course = {
+    name: 'Half Stack application development',
+    parts: [
+        {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+        },
+        {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+        },
+        {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+        }
+    ]
+    }
+
+    return (
+    <div>
+        <Course course={course} />
+    </div>
+    )
+}
+
+export default App
