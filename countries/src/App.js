fb014b1585592db207e97de1e0407992e2ee347d
ponko23 <kohe.mizu@gmail.com>
2019-09-09 00:16:55 +0900

    2.14 finished?

--- countries/src/App.js
+++ countries/src/App.js
@@ -12,7 +12,16 @@ const Languages = ({languages}) =>
     </ul>
   </>
 
-const Country = ({ country }) => country === null
+const Forcast = ({capital, forcast }) => forcast === null
+  ? ''
+  : <div>
+      <h3>Weather in {capital}</h3>
+      <b>temperature:</b> {forcast.temp_c} Celsius<br />
+      <img src={forcast.condition} alt={forcast.condition} />
+      <b>wind:</b> {forcast.wind_kph} kph direction {forcast.wind_dir}<br />
+    </div>
+
+const Country = ({ country, forcast }) => country === null
   ? ''
   : <div>
       <h2>{country.name}</h2>
@@ -20,6 +29,7 @@ const Country = ({ country }) => country === null
       population {country.population}<br />
       <Languages languages={country.languages} />
       <img src={country.flag} alt={`${country.name}'s flag`} width='200' />
+      <Forcast capital={country.capital} forcast={forcast} />
     </div>
 
 
@@ -27,6 +37,8 @@ const App = () => {
   const [ searchKey, setSearchKey ] = useState('')
   const [ countries, setCountries ] = useState([])
   const [ selectedCountry, setSelectedCountry ] = useState(null)
+  const [ forcast, setForcast ] = useState(null)
+
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
@@ -35,6 +47,17 @@ const App = () => {
       })
   }, [])
   
+  useEffect(() => {
+    if(selectedCountry !== null)
+    {
+      axios
+      .get(`http://api.apixu.com/v1/current.json?key=<YOUR_API_KEY>&q=${selectedCountry.capital}`)
+      .then(response => {
+        setForcast(response.data)
+      })
+    }
+  }, [selectedCountry])
+
   const changeCountry = (country) => setSelectedCountry(country)
 
   const showCountries = searchKey === ''
@@ -58,12 +81,10 @@ const App = () => {
     : showCountries.map(country => <Row key={country.name} country={country} changeCountry={changeCountry} />)
   
   const country = () => {
-    if(selectedCountry !== null) {
-      return <Country country={selectedCountry} />
-    } else if(showCountries.length === 1) {
-      return <Country country={showCountries[0]} />
+    if(selectedCountry === null && showCountries.length === 1) {
+      setSelectedCountry(showCountries[0]) 
     }
-    return ''
+    return <Country country={selectedCountry} forcast={forcast} />
   }
 
   return (
